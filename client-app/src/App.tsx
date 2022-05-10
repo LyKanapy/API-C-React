f28759d4d9fb6bc1d7020293301126920783b521
BigToeEnergy <l.kanapickas@gmail.com>
2022-05-10 21:39:27 +0300

    220510

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((res) => {
+      console.log(res);
+      setActivities(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
